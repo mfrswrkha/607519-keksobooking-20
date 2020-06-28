a6e9fe2428395a536d7a14e4833b69eaca6b696c
mfrswrkha <mfrswrk@yandex.ru>
2020-06-28 12:00:35 +0300

    update from master, used modules

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 'use strict';
 var countOfOffers = 8;
 var offers = window.data.createUserOffers(countOfOffers);
-// eslint-disable-next-line no-console
-console.log(offers);
 window.map.setMapPins(offers);
 window.card.setCard(offers, 0);
 window.common.setStateInit();
