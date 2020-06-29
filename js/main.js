f26b471e5162ebbacc22a66b302311ddfd289c7f
mfrswrkha <mfrswrk@yandex.ru>
2020-06-29 18:00:14 +0300

    resolved conflicts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+//update for resolve merge conflict
 (function () {
   var url = 'https://javascript.pages.academy/keksobooking/data';
   window.xhr.loadData(url, window.map.setMapPins);
