0e264adb2b4e381e53b8c76e5e8708de6e34bf69
mfrswrkha <mfrswrk@yandex.ru>
2020-06-30 23:53:29 +0300

    add filter for housing-type

--- js/common.js
+++ js/common.js
@@ -1,23 +1,71 @@
+/* eslint-disable no-console */
 'use strict';
 (function () {
   var adForm = document.querySelector('.ad-form');
-  var adFormInputs = adForm.querySelectorAll('input');
-  var adFormSelects = adForm.querySelectorAll('select');
+  var filterForm = document.querySelector('.map__filters');
+  var adFormInputsSelects = adForm.querySelectorAll('input,select');
+  var filterFormInputsSelects = filterForm.querySelectorAll('input,select');
   window.common = {
     setStateInit: function () {
       adForm.classList.add('ad-form--disabled');
-      setAttributeForListFields(adFormInputs, 'disabled', '');
-      setAttributeForListFields(adFormSelects, 'disabled', '');
+      setAttributeForListFields(adFormInputsSelects, 'disabled', 'disabled');
+      setAttributeForListFields(filterFormInputsSelects, 'disabled', 'disabled');
       window.form.setAddressFromPin(adForm, false);
     },
     setStateActive: function () {
       adForm.classList.remove('ad-form--disabled');
-      removeAttributeForListFields(adFormInputs, 'disabled');
-      removeAttributeForListFields(adFormSelects, 'disabled');
+      removeAttributeForListFields(adFormInputsSelects, 'disabled');
+      console.log(window.main.offers);
+      if (window.main.offers.length > 0) {
+        console.log('manyOffers', window.map.mapPins);
+        removeAttributeForListFields(filterFormInputsSelects, 'disabled');
+      }
       window.map.offersMap.classList.remove('map--faded');
       window.form.setAddressFromPin(adForm, true);
       window.form.fillAdForm();
+    },
+    filterOffers: function (size) {
+      function isSomeHouseType(element) {
+        if (filterHouseType.value === 'any') {
+          return element;
+        } else if (filterHouseType.value === element.offer.type) {
+          return element;
+        }
+        return null;
+      }
+      var filterHouseType = filterForm.querySelector('#housing-type');
+      var index = 0;
+      var childrenCount = window.map.mapPins.children.length;
+      for (var i = 0; i < childrenCount; i++) {
+        if (window.map.mapPins.children[index].type === 'button') {
+          window.map.mapPins.removeChild(window.map.mapPins.children[index]);
+        } else {
+          index++;
+        }
+      }
+      var result = window.main.offers.filter(isSomeHouseType);
+      switch (result.length) {
+        case (result.length > size): {
+          var topSizeList = result.slice(0, size);
+          window.map.setMapPins(topSizeList);
+          break;
+        }
+        case (result.length === 0): {
+          console.log('not found result');
+          break; }
+        default:
+          window.map.setMapPins(result);
+      }
+    },
+    listenFilterHouseType: function (size) {
+      var filterHouseType = filterForm.querySelector('#housing-type');
+      filterHouseType.addEventListener('click', this.filterOffers(size));
+      filterHouseType.addEventListener('change', function () {
+        var card = window.map.offersMap.querySelector('article');
+        card.remove();
+      });
     }
+
   };
   function setAttributeForListFields(element, attribute, value) {
     for (var i = 0; i < element.length; i++) {
--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 'use strict';
 (function () {
   var url = 'https://javascript.pages.academy/keksobooking/data';
-  window.xhr.loadData(url, window.map.setMapPins);
-  window.common.setStateInit();
-  window.pin.listenPinForNewOfferMouse();
-  window.pin.listenPinForNewOfferKey();
+  var sizeOffersList = 5;
+  window.main = {
+    runBasicLogic: runBasicLogic,
+    offers: []
+  };
+
+  window.xhr.loadData(url, runBasicLogic);
+  function runBasicLogic(sourceData) {
+    window.main.offers = sourceData.slice(0);
+    // eslint-disable-next-line no-console
+    console.log(window.main.offers);
+    window.common.setStateInit();
+    window.map.setMapPins(window.main.offers);
+    window.pin.listenPinForNewOfferMouse();
+    window.pin.listenPinForNewOfferKey();
+    window.card.setCard(window.main.offers, 0);
+    window.common.filterOffers(sizeOffersList);
+    window.common.listenFilterHouseType(sizeOffersList);
+  }
 })();
